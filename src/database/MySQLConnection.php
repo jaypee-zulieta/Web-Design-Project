bd10a94fffad060c551538241519828e9e96954a
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2025-08-18 13:45:42 +0800

    feat: Add MySQLConnectionConfiguration class
    It is a class that creates a MySQL connection
    using environment variables within the system;
    DB_HOST, DB_USER, DB_PASSWORD and DB.

---
+++ src/database/MySQLConnection.php
@@ -0,0 +1,35 @@
+<?php
+namespace DatabaseConnection;
+
+use mysqli;
+
+class MySQLConnectionConfiguration {
+ 
+  private static MySQLConnectionConfiguration | null $instance = null;
+  private mysqli $connection;
+
+  private function __construct()
+  {
+    $this->connection = new mysqli(
+      getenv("DB_HOST"),
+      getenv("DB_USER"),
+      getenv("DB_PASSWORD"),
+      getenv("DB")
+    );
+    
+    if($this->connection->connect_errno)
+      die("Connection failed! " . $this->connection->connect_error);
+  }
+
+  public static function getInstance(): MySQLConnectionConfiguration {
+    if(self::$instance === null) 
+      self::$instance = new MySQLConnectionConfiguration();
+    return self::$instance;
+  }
+
+  public function getConnection(): mysqli {
+    return $this->connection;
+  }
+}
+
+?>
